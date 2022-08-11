25c7606ba0720f16492fca0114b609b43467ba9f
lingtine <lingtine@gmail.com>
2022-08-12 00:37:26 +0700

    upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/icons/fontawesome-free-6.1.2-web/css/all.css">
+</head>
+<body>
+    <div class="warper">
+        <div class="header">
+            <img class="header-img" src="https://scontent.fsgn5-14.fna.fbcdn.net/v/t1.6435-9/41575414_1882851371796955_8622048126896701440_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CA4MOeUrGsEAX_1uNVA&_nc_ht=scontent.fsgn5-14.fna&oh=00_AT-TU6i32LLfAc7zqHaNYOMVi_F2pWQCuN_6WNLNky53kA&oe=631BD865" />
+            <ul class="navbar">
+                <li class="navbar-item">
+                    <a href="#" class="navbar-item--href">Giới thiệu</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="navbar-item--href">Sản phẩm</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="navbar-item--href">Khuyến mãi</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="navbar-item--href">Tin tức</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="navbar-item--href">Tuyển dụng</a>
+                </li>
+            </ul>
+            <div class="action">
+                <div class="cart">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+                <div class="user-login">
+                    <i class="fa-solid fa-user"></i>
+                </div>
+
+            </div>
+        </div>
+        <div class="container"></div>
+        <div class="footer"></div>
+    </div>
+</body>
+</html>
